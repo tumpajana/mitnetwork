7e4bb194684914817ef1f2885f12319165ce45dd
payelduttameme <payel.dutta@memeinfotech.com>
2018-03-07 11:40:04 +0530

    component added

---
+++ Components/Signup/Signup.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './Signup.css';
+
+class Signup extends Component {
+  render() {
+    return (
+      <div className="App">
+       
+      </div>
+    );
+  }
+}
+
+export default Signup;
