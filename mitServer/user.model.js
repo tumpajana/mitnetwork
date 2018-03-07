a6a11917fb6e92951b78766582ff1eea99620098
meme-neha <neha.keshri@memeinfotech.com>
2018-03-07 13:05:51 +0530

    api for registration

---
+++ mitServer/user.model.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+const userSchema = mongoose.Schema({
+
+    userName: {
+        type: String
+    },
+    firstName: {
+        type: String
+    },
+    lastName: {
+        type: String
+    },
+    phoneNumber: {
+        type: String,
+        unique: true
+    },
+    email: {
+        type: String,
+        unique: true
+    },
+    password: {
+        type: String
+    },
+    createdDate: {
+        type: Date,
+        default: Date.now
+    },
+    updatedDate: {
+        type: Date,
+        default: Date.now
+    },
+    enabled: {
+        type: Number,
+        default: 1
+    }
+});
+const user = module.exports = mongoose.model('user', userSchema);
