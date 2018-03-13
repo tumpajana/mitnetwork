1b2a9affd63e65825c71a95ed25d6d170e3e0080
Samikshameme <samiksha.sonam@memeinfotech.com>
2018-03-13 10:37:54 +0530

    toast implemented

--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -10,7 +10,11 @@ import { Redirect } from 'react-router-dom';
 import PostData from '../../Services/signupapi';
 import FacebookloginData from '../../Services/socialapi';
 import { browserHistory } from 'react-router';
+import { ToastContainer, toast } from 'react-toastify';
+
 const RadioGroup = Radio.Group;
+
+
 
 class Signup extends Component {
 
@@ -45,10 +49,11 @@ class Signup extends Component {
 
     };
 
+
+
     this.register = this.register.bind(this);
     this.onChangeValue = this.onChangeValue.bind(this);
     this.facebookLogin = this.facebookLogin.bind(this);
-
   }
 
   responseFacebook = (response) => {
@@ -217,204 +222,218 @@ class Signup extends Component {
 
 
 
+  //submit registration form
+  register = () => {
+    console.log('submit button');
+    console.log(this.state.name)
+    // if(this.handleValidation()){     // validation function
 
-
-    //submit registration form
-    register = () => {
-      console.log('submit button');
-      console.log(this.state.name)
-      // if(this.handleValidation()){     // validation function
-
-      if (this.state.userName && this.state.password && this.state.email && this.state.name && this.state.phoneNumber) {
-        PostData(this.state).then((result) => {
-          let response = result;
-          console.log(result)
-          if (response.userData) {
-            sessionStorage.setItem('userData', JSON.stringify(response));
-            this.setState({ redirectToReferrer: true });
-
-          }
-
-        });
-      }
-      //else{
-      // alert("Form has errors.")
-      // }
-
-    }
-
-    facebookLogin = (res, type) => {
-      FacebookloginData(this.state.facebookInfo).then((result) => {
+    if (this.state.userName && this.state.password && this.state.email && this.state.name && this.state.phoneNumber) {
+      PostData(this.state).then((result) => {
         let response = result;
-        console.log(response)
+        console.log(result);
+        if(result.error==false){
+           toast.success("You have been registered successfully!", {
+          position: toast.POSITION.TOP_CENTER,
+        });
         if (response.userData) {
-          sessionStorage.setItem('loginData', JSON.stringify(response));
+          sessionStorage.setItem('userData', JSON.stringify(response));
           this.setState({ redirectToReferrer: true });
+
         }
+        else {
+           toast.warn("num ber already exist!", {
+          position: toast.POSITION.TOP_CENTER,
+        });
+        }
+        }
+       
 
       });
     }
+    //else{
+    // alert("Form has errors.")
+    // }
 
-    render() {
-      if (this.state.redirectToReferrer) {
-        return <Redirect to="/Profile" />
+
+  }
+
+  facebookLogin = (res, type) => {
+    FacebookloginData(this.state.facebookInfo).then((result) => {
+      let response = result;
+      console.log(response)
+      if (response.userData) {
+        sessionStorage.setItem('loginData', JSON.stringify(response));
+        this.setState({ redirectToReferrer: true });
       }
-      const { userName } = this.state;
 
-      const suffix = userName ? <Icon type="close-circle" onClick={this.emitEmpty} /> : null;
-      return (
-        <div className="signuparea">
-          <div className="signupcard">
-            <Row type="flex" justify="center">
-              <Col lg={9} sm={0} xs={0}>
+    });
+  }
 
-                <div className="sidesection">
-                  <img src={mitlogo} />
-                  <h2>Welcome To MIT Social</h2>
-                  <hr />
+  render() {
+    if (this.state.redirectToReferrer) {
+      return <Redirect to="/Profile" />
+    }
+    const { userName } = this.state;
+
+    const suffix = userName ? <Icon type="close-circle" onClick={this.emitEmpty} /> : null;
+    return (
+      <div className="signuparea">
+        <div className="signupcard">
+          <Row type="flex" justify="center">
+            <Col lg={9} sm={0} xs={0}>
+
+              <div className="sidesection">
+                <img src={mitlogo} />
+                <h2>Welcome To MIT Social</h2>
+                <hr />
+              </div>
+
+            </Col>
+            <Col lg={15} sm={24} xs={24} className="centercontent">
+              <div className="formsigninmit1">
+                <div className="formarea">
+                  <div className="formheading">
+                    <p className="signfont">Sign Up </p>
+                  </div>
                 </div>
 
-              </Col>
-              <Col lg={15} sm={24} xs={24} className="centercontent">
-                <div className="formsigninmit1">
-                  <div className="formarea">
-                    <div className="formheading">
-                      <p className="signfont">Sign Up </p>
-                    </div>
-                  </div>
-                  <Row type="flex" justify="center">
+                <Row type="flex" justify="center">
 
-                    <Col lg={10} sm={10} xs={24}>
-                      <form className="formsinput">
-                        <Input
-                          ref="name"
-                          placeholder="Your Name"
-                          name="name"
-                          prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                          suffix={suffix}
-                          // value={userName}
-                          // onChange={this.onChangeUserName}
-                          // onChange={this.handleChange.bind(this, "name")} 
-                          //value={this.state.fields["name"]}
-                          onChange={this.onChangeValue}
-                        // ref={node => this.userNameInput = node}
+                  <Col lg={10} sm={10} xs={24}>
+                    <form className="formsinput">
+                      <Input
+                        ref="name"
+                        placeholder="Your Name"
+                        name="name"
+                        prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                        suffix={suffix}
+                        // value={userName}
+                        // onChange={this.onChangeUserName}
+                        // onChange={this.handleChange.bind(this, "name")} 
+                        //value={this.state.fields["name"]}
+                        onChange={this.onChangeValue}
+                      // ref={node => this.userNameInput = node}
 
-                        />
-                        <div> {this.state.valid.nameText} </div>
-                        <Input
-                          placeholder="Username"
-                          name="userName"
-                          prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                      />
+                      <div> {this.state.valid.nameText} </div>
+                      <Input
+                        placeholder="Username"
+                        name="userName"
+                        prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
 
-                          onChange={this.onChangeValue}
+                        onChange={this.onChangeValue}
 
-                        />
-                        <div> {this.state.valid.nameText1} </div>
-                        {/* <Input
+                      />
+                      <div> {this.state.valid.nameText1} </div>
+                      {/* <Input
                       placeholder="Username"
                       prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
                     
                     /> */}
-                        {/* <p class="genderchoose"> Gender</p>
+                      {/* <p class="genderchoose"> Gender</p>
                     <RadioGroup onChange={this.onChange} value={this.state.value}>
                       <Radio value={1} className="gendervalue">Male</Radio>
                       <Radio value={2} className="gendervalue">Female</Radio>
                     </RadioGroup> */}
-                        <Input
-                          placeholder=" Email"
-                          name="email"
-                          prefix={<Icon type="mail" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                          //<input ref="name" type="text" size="30" placeholder="Name" onChange={this.handleChange.bind(this, "name")} value={this.state.fields["name"]}/>
-                          onChange={this.onChangeValue}
+                      <Input
+                        placeholder=" Email"
+                        name="email"
+                        prefix={<Icon type="mail" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                        //<input ref="name" type="text" size="30" placeholder="Name" onChange={this.handleChange.bind(this, "name")} value={this.state.fields["name"]}/>
+                        onChange={this.onChangeValue}
 
-                        />
-                        <div> {this.state.valid.nameText2} </div>
-                        <Input
-                          placeholder=" Phone Number"
-                          name="phoneNumber"
-                          prefix={<Icon type="phone" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                      />
+                      <div> {this.state.valid.nameText2} </div>
+                      <Input
+                        placeholder=" Phone Number"
+                        name="phoneNumber"
+                        prefix={<Icon type="phone" style={{ color: 'rgba(0,0,0,.25)' }} />}
 
-                          onChange={this.onChangeValue}
-                        // value={this.state.fields["phoneNumber"]}
-                        />
-                        <div> {this.state.valid.nameText3} </div>
-                        <Input
-                          placeholder=" Password"
-                          name="password"
-                          name="password"
-                          prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                        onChange={this.onChangeValue}
+                      // value={this.state.fields["phoneNumber"]}
+                      />
+                      <div> {this.state.valid.nameText3} </div>
+                      <Input
+                        placeholder=" Password"
+                        name="password"
+                        name="password"
+                        prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
 
-                          onChange={this.onChangeValue}
+                        onChange={this.onChangeValue}
 
-                        />
-                        <div> {this.state.valid.nameText4} </div>
-                        <Input
-                          placeholder="Confirm Password"
-                          prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                          name="confirmPassword"
+                      />
+                      <div> {this.state.valid.nameText4} </div>
+                      <Input
+                        placeholder="Confirm Password"
+                        prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                        name="confirmPassword"
 
-                          onChange={this.onChangeValue}
-                        />
-                        <div> {this.state.valid.nameText5} </div>
+                        onChange={this.onChangeValue}
+                      />
+                      <div> {this.state.valid.nameText5} </div>
 
-                      </form>
-                    </Col>
-                    <Col lg={2} sm={2} xs={0}>
+                    </form>
+                  </Col>
+                  <Col lg={2} sm={2} xs={0}>
 
-                      <div className="wrapper">
-                        <div className="line"></div>
-                        <div className="wordwrapper">
-                          <div className="ordivider">OR</div>
-                        </div>
+                    <div className="wrapper">
+                      <div className="line"></div>
+                      <div className="wordwrapper">
+                        <div className="ordivider">OR</div>
                       </div>
-                    </Col>
-                    <Col lg={12} sm={12} xs={24} className="sociallogin">
+                    </div>
+                  </Col>
+                  <Col lg={12} sm={12} xs={24} className="sociallogin">
 
-                      <div className="signupwithsocial">
-                        <p className="ordividerres">OR</p>
-                        {/* <Button className="facebooklogin">Sign in 
+                    <div className="signupwithsocial">
+                      <p className="ordividerres">OR</p>
+                      {/* <Button className="facebooklogin">Sign in 
                   <Icon type="facebook" />
                   </Button>
                   <Button className="googlepluslogin">Sign in 
                   <Icon type="google-plus" />
                   </Button> */}
-                        <FacebookLogin
-                          appId="312775355854012"
-                          autoLoad={true}
-                          fields="name,email,picture"
-                          // onClick={componentClicked}
-                          callback={this.responseFacebook}
-                          className="facebooksignin"
-                          icon="fa-facebook" />
-                        <GoogleLogin
-                          clientId="1039315261739-cesl5gtd6vqk00bancklm039rcjo3orq.apps.googleusercontent.com"
-                          buttonText="Login"
-                          className="googleplussign"
-                          onSuccess={this.responseGoogle}
-                          onFailure={this.responseGoogle}
-                          icon="google-plus"
+                      <FacebookLogin
+                        appId="312775355854012"
+                        autoLoad={true}
+                        fields="name,email,picture"
+                        // onClick={componentClicked}
+                        callback={this.responseFacebook}
+                        className="facebooksignin"
+                        icon="fa-facebook" />
+                      <GoogleLogin
+                        clientId="1039315261739-cesl5gtd6vqk00bancklm039rcjo3orq.apps.googleusercontent.com"
+                        buttonText="Login"
+                        className="googleplussign"
+                        onSuccess={this.responseGoogle}
+                        onFailure={this.responseGoogle}
+                        icon="google-plus"
 
-                        />
-                      </div>
-
-                    </Col>
-
-                    <div className="registerbtn">
-                      <Button className="sbmtbtn" onClick={this.register}>Submit</Button>
-                      <Button className="cnclbtn">Cancel</Button>
-                      <p className="regtext"> Already Registered ? &nbsp;&nbsp;<a className="loginlink" href='/Signin' >Login</a> &nbsp;here</p>
+                      />
                     </div>
 
+                  </Col>
 
-                  </Row>
-                </div>
-              </Col>
+                  <div className="registerbtn">
+                    <Button className="sbmtbtn" onClick={this.register}>Submit</Button>
+                    <Button onClick={this.closeAfter15}>Close after 15 seconds</Button>
+                    <Button className="cnclbtn">Cancel</Button>
 
-            </Row>
-          </div>
+                    <p className="regtext"> Already Registered ? &nbsp;&nbsp;<a className="loginlink" href='/Signin' >Login</a> &nbsp;here</p>
+                  </div>
+
+
+                </Row>
+              </div>
+            </Col>
+
+          </Row>
         </div>
-      );
-    }
+        <ToastContainer autoClose={2000} />
+      </div>
+    );
   }
+}
 
-  export default Signup
+export default Signup
